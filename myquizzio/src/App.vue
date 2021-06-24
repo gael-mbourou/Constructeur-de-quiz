909b76479e3576081e9888090d85788773152912
Gaël MBOUROU-DIYEKI <gaelmbourou11@gmail.com>
2021-06-24 14:37:09 +0200

    mise à jour du message de bienvenu

--- myquizzio/src/App.vue
+++ myquizzio/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Bienvenu sur le site de quizz"/>
   </div>
 </template>
 
