5a5c69746884741631ba9e922e7984b57c31142c
Gaël MBOUROU-DIYEKI <gaelmbourou11@gmail.com>
2021-06-24 20:18:40 +0200

    Ajout amélioration de la structure précédente

---
+++ myquizzio/src/components/HeaderApp.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <logo-app></logo-app>
+  </div>
+</template>
+
+<script>
+import LogoApp  from './LogoApp.vue'
+//import HeaderLogo from './HeaderLogo.vue'
+
+export default{
+    name:'HeaderApp',
+    components:{
+        'logo-app': LogoApp
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ myquizzio/src/components/LogoApp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <img  alt="Vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<script>
+export default {
+    name:'LogoApp',
+}
+</script>
+
+<style scoped>
+  img {
+   width: 70px;
+  }
+</style>
